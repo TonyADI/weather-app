d02915487c7bb0c9f59b6b20a1b7f5baf6881e13
Emmanuel <35707107+TonyADI@users.noreply.github.com>
2022-03-28 15:21:03 -0400

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,11 +16,8 @@ const App = () => {
     const [condition, setCondition] = useState('');
     const [time, setTime] = useState('');
     const [imageUrl, setImageUrl] = useState(clear);
-    const [color, setColor] = useStae('#fff');
+    const [color, setColor] = useState('#fff');
     const [days, setDays] = useState([]);
-      this.state = {temperature: '', city: '', condition: '', time: '', 
-      imageUrl:clear, color: '#fff', firstDay: {}, secondDay: {}, thirdDay: {}, 
-      fourthDay: {}, fifthDay: {}}
 
     const startClock = () => {
       let time = new Date().toLocaleTimeString();
@@ -51,69 +48,16 @@ const App = () => {
       }
     }
 
-    const getMinTemp = list => {
-      let min = Number.MAX_VALUE;
-      for(let i = 0; i < 8; i++){
-        if(list[i].main.temp < min){
-            min = list[i].main.temp;
-        }
-      }
-      return min;
-    }
-
-    const getMaxTemp = list => {
-      let max = 0;
-      for(let i = 0; i < 8; i++){
-        if(list[i].main.temp > max){
-            max = list[i].main.temp;
-        }
-      }
-      return max;
-    }
-
-    const getCondition = list => {
-      let frequency = {};
-      let max = 0;
-      let condition;
-      for(let index in list) {
-        frequency[list[index].weather[0].main] = 
-        (frequency[list[index].weather[0].main] || 0) + 1;
-        if(frequency[list[index].weather[0].main] > max) {
-          max = frequency[list[index].weather[0].main];
-          condition = list[index].weather[0].main;
-        }
-      }
-      return condition;
-    }
-
     const handleForecast = (long, lat) => {
-      if(long && lat){
-        getForecast(long, lat).then(forecast => {
-          setCity(forecast.city);
-          this.setState({city: forecast.city, temperature: Math.floor(forecast.forecasts[0].main.temp), 
-            condition: forecast.forecasts[0].weather[0].main, 
-          firstDay: {max: Math.floor(this.getMaxTemp(forecast.forecasts.slice(0,8))), min:
-          Math.floor(this.getMinTemp(forecast.forecasts.slice(0,8))), 
-          condition: this.getCondition(forecast.forecasts.slice(0,8))},
-          secondDay: {max: Math.floor(this.getMaxTemp(forecast.forecasts.slice(8,16))), min:
-            Math.floor(this.getMinTemp(forecast.forecasts.slice(8,16))), 
-          condition: this.getCondition(forecast.forecasts.slice(8,16))
-          },
-          thirdDay: {max: Math.floor(this.getMaxTemp(forecast.forecasts.slice(16,24))), min:
-            Math.floor(this.getMinTemp(forecast.forecasts.slice(16,24))), 
-            condition: this.getCondition(forecast.forecasts.slice(16,24))
-          },
-          fourthDay: {max: Math.floor(this.getMaxTemp(forecast.forecasts.slice(24,32))), min:
-            Math.floor(this.getMinTemp(forecast.forecasts.slice(24,32))),
-            condition: this.getCondition(forecast.forecasts.slice(24,32))
-          },
-          fifthDay: {max: Math.floor(this.getMaxTemp(forecast.forecasts.slice(32,40))), min:
-            Math.floor(this.getMinTemp(forecast.forecasts.slice(32,40))),
-            condition: this.getCondition(forecast.forecasts.slice(32,40))
-          }});
-        })
-        .catch(error => console.log(error));
-    }
+        if(long && lat){
+            getForecast(long, lat).then(forecast => {
+            setCity(forecast.city);
+            setDays(forecast.days);
+            setTemperature(forecast.temp);
+            setCondition(forecast.condition);
+            })
+            .catch(error => console.log(error));
+        }
     }
 
     const handleSearch = term => {
